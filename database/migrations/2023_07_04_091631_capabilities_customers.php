77ca54bfe40205e52534eed8ffb4ce8958399305
LAPTOP-M9KEM7H7\Marty <martyumlas@gmail.com>
2023-07-04 17:23:18 +0800

    create models, relationship methods and migration for customer, address and ccapabilities

---
+++ database/migrations/2023_07_04_091631_capabilities_customers.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capabilities_customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('capability_id');
+            $table->string('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
